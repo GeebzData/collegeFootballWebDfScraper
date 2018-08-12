b39141047474bd83989c54f5b37dd76a476aed99
Geebz <41347453+GeebzData@users.noreply.github.com>
2018-08-12 14:31:49 -0600

    Update PennStateFbScrape.py
    syntax updates

--- PennStateFbScrape.py
+++ PennStateFbScrape.py
@@ -2,22 +2,20 @@ import csv
 import pandas as pd
 
 # create column headers for final df
-col = ['Game Number', 'Date', 'Location', 'Opponent', 'Result', 'Passing Completion', 'Passing Attempts', 'Passing Percentage', 'Passing Yards', 'Passing TDs', 'RushingA ttempts', 'Rushing Yards', 'Rushing Average', 'Rushing TDs', 'Offensive Plays', 'Offensive Yards', 'Offensive Average', 'First Down Pass', 'First Down Rush', 'First Down Penalties', 'First Down Total', 'Penalties, 'Penalty Yards', 'Fumbles', 'Interceptions', 'Total Turnovers']
-
-read = pd.read_html('https://www.sports-reference.com/cfb/schools/penn-state/201{}/gamelog/'.format(i), header=0)
+col = ['Game Number', 'Date', 'Location', 'Opponent', 'Result', 'Passing Completion', 'Passing Attempts', 'Passing Percentage', 'Passing Yards', 'Passing TDs', 'RushingA ttempts', 'Rushing Yards', 'Rushing Average', 'Rushing TDs', 'Offensive Plays', 'Offensive Yards', 'Offensive Average', 'First Down Pass', 'First Down Rush', 'First Down Penalties', 'First Down Total', 'Penalties', 'Penalty Yards', 'Fumbles', 'Interceptions', 'Total Turnovers']
 
 # iterate over web pages from years 2015-2018, once past 2015, move to the else statement
 for i in range(5,8):
 # iterate through first web page to pull the offensive df
     if i == 5:
-        psu = read
+        psu = pd.read_html('https://www.sports-reference.com/cfb/schools/penn-state/201{}/gamelog/'.format(i), header=0)
         # select first df on page and remove bottom row and header
         psu_season = psu[0].iloc[1:-1:]
         # create header - col
         psu_season.columns = col
 # iterate through every other web page to pull the offensive df
     else:
-        psu = read
+        psu = pd.read_html('https://www.sports-reference.com/cfb/schools/penn-state/201{}/gamelog/'.format(i), header=0)
         psu_season_data = psu[0].iloc[1:-1:]
         psu_season_data.columns = col
         # concat original & appended df with new df
